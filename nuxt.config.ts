c8df1669e4635c9f76c961cba38e808f59719b22
harsh <behlharsh10@gmail.com>
2025-05-17 21:25:00 +0530

    chore: init Nuxt SaaS template with Studio preview

---
+++ nuxt.config.ts
@@ -0,0 +1,49 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  content: {
+    // enable Studio’s live-preview API
+    preview: {
+      api: 'https://api.nuxt.studio'
+    }
+  },                  // ← comma added here
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui-pro',
+    '@nuxt/content',
+    '@vueuse/nuxt',
+    'nuxt-og-image'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/docs': { redirect: '/docs/getting-started', prerender: false }
+  },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  compatibilityDate: '2024-07-11',
+
+  nitro: {
+    prerender: {
+      routes: ['/'],
+      crawlLinks: true
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
